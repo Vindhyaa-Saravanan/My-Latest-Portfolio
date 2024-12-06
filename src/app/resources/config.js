c5618d48309b48a004287bbd5d4ae15af3733629
Vindhyaa Saravanan <vindhyaa.saravanan@yahoo.co.in>
2024-12-06 11:47:55 +0000

    Fix

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -16,6 +16,21 @@ const routes = {
     '/gallery': true,
 }
 
+const effects = {
+    mask: 'cursor',             // none | cursor | topLeft | topRight | bottomLeft | bottomRight
+    gradient: {
+        display: true,
+        opacity: 0.4            // 0 - 1
+    },
+    dots: {
+        display: true,
+        opacity: 0.4,           // 0 - 1
+        size: '24'              // 2 | 4 | 8 | 12 | 16 | 20 | 24 | 32 | 40 | 48 | 56 | 634
+    },
+    lines: {
+        display: false,
+    },
+}
 
 const style = {
     theme:       'dark',         // dark | light
